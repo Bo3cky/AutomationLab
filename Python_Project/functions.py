2e4abccdf41a75139a51d2bf37c0c9e542f51d15
Bo3cky <85841056+Bo3cky@users.noreply.github.com>
2021-07-16 16:13:44 +0200

    Update functions.py

--- Python_Project/functions.py
+++ Python_Project/functions.py
@@ -1,29 +1,34 @@
-""" Ce programme contient toutes les fonctions qui seront appelées par le programme Menu.py"""
-
 #!/usr/bin/python3
 """ Ce programme contient toutes les fonctions qui seront appelées par le programme Menu.py"""
-
+import os
 import subprocess
 import platform
+from filecompare import file_analysis
+import getpass
+import hashlib
 
-os = platform.system()
-### FONCTIONS DU MENU NETWORK_AUTOAMTION ###
+OS = platform.system()
+
+### FONCTIONS DU MENU NETWORK_AUTOAMTION ###  (en cours de développement)
 def gather_ip():
-    if os == "Windows":
+    if OS == "Windows":
       print(subprocess.run(args="ipconfig /all", shell=True))
-    elif os == "Linux":
+    elif OS == "Linux":
       print(subprocess.run(args="ip a", shell=True))
       exit(0)
-
 
 def gather_mac():
     print(subprocess.run(args="getmac /v", shell=True))
     exit(0)
 
-def net_test():
-    print(subprocess.run(args="ansible -i ""inventaire.ini"" all -m ping", shell=True))
+def ssh_deploy_key():
+    print(subprocess.run(args="ansible-playbook -i inventaire.ini gendistkey-ssh.yml", shell=True))
     exit(0)
 
 ### FONCTIONS DU MENU SYSTEM_AUTOAMTION ###
 
-### FONCTIONS DU MENU SECURITY_AUTOAMTION #
+def gathering_file():
+    print(subprocess.run(args=" ansible-playbook -i inventaire.ini Checksum-file.yml | ./tri.sh ", shell=True ))
+    file_analysis()
+    exit(0)
+### FONCTIONS DU MENU SECURITY_AUTOAMTION ### (en cours de développement)
