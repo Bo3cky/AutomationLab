5e9b7df584c9a111fb492d485e4478c3b9ce5789
Bo3cky <85841056+Bo3cky@users.noreply.github.com>
2021-07-23 11:36:46 +0200

    Update tri.sh

--- Python_Project/tri.sh
+++ Python_Project/tri.sh
@@ -1,3 +1,4 @@
-#/usr/bin/bash
+#!/usr/bin/bash
+# Script permettant la mise en forme des données récupérées par la tâche n°2 du menu SYSTEM AUTOMATION (Gathering-file-checksum)
 
 tee /dev/tty | grep -E '/etc/' | awk  '{print "File: "$6,"\tHost:node1 ","\tChecksum: "$8}' > checksum.txt ; sed -i '2s/node1/node2/g' checksum.txt
